8eb39481619e0234e4965020614f90e326bebb0f
alapod <103041213+alapod@users.noreply.github.com>
2022-10-01 13:29:15 +0500

    Update settings.py

--- mysite_redone/settings.py
+++ mysite_redone/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q8wz_&gef*bd8=eb*j9&gw1(%s68ug4id+931!%i-jj5*4o%6_'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
